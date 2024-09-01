8fd42803c2dd7934cb2b97b66b816fb0c5456046
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2024-09-01 19:31:54 -0300

    added github pages

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ export default ({
   template: Deno.readTextFileSync('src/template.html'),
   title: 'NosTracker',
   description: 'A website dedicated to collecting information from the nostr network.',
-  url: 'http://localhost:3000/',
+  url: 'https://marcodpt.github.io/nostracker/',
   dir: 'docs',
   rss: false
 })
